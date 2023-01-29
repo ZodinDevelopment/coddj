34b9f76cf9c01ea2c5e22174a61f241277913a1f
Dylan Garrett <glockswitch@core>
2023-01-28 19:46:04 -0600

    now supports playlist txt file randomization

--- src/coddj/coddj.py
+++ src/coddj/coddj.py
@@ -1,31 +1,76 @@
 from __future__ import print_function, unicode_literals
 
 from PyInquirer import prompt
-
+import random
 from search import search
 from player import play_song
 from pyfiglet import Figlet
 
 
 EXIT_TOGGLE = False
-
+queue = []
 
 def main():
+    
     questions = [
         {
-            "type": "input",
-            "name": "query",
-            "message": "Search: ",
+            "type": "list",
+            "name": "mode",
+            "message": "Mode: ",
+            "choices": ["search", "playlist"],
         }
     ]
-    query = prompt(questions)
-    search_results = search(query.get("query"))
+    mode = prompt(questions).get("mode")
+    if mode == "search":
+      query = query_mode()
+    elif mode == "playlist":
+      get_file_name()
+      query = shuffle_playlist()
+    search_results = search(query)
 
     choice = list_search_results(search_results)
 
     play_song(choice['search'])
 
     return prompt_to_continue()
+
+
+def get_file_name():
+  questions = [
+    {
+      "type": "input",
+      "name": "filename",
+      "message": "Playlist File: ",
+    }
+  ]
+  filename = prompt(questions).get("filename")
+  with open(filename, 'r') as f:
+    data = f.read()
+  data = data.split("\n")
+  for line in data:
+    queue.append(line)
+  #return filename.get("filename")
+  
+  
+def shuffle_playlist():
+  if len(queue) > 0:
+    choice = random.choice(queue)
+    queue.remove(choice)
+    return choice
+  else:
+    return ""
+
+
+def query_mode():
+  questions = [
+    {
+      "type": "input",
+      "name": "query",
+      "message": "Search: ",
+    }  
+  ]
+  query = prompt(questions)
+  return query.get("query")
 
 
 def list_search_results(search_list):
